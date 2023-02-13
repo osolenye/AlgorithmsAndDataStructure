87ec179dd47355027fd87c0d00611a2c55dfde13
osolenye <apasniynegr228@gmail.com>
2023-02-13 22:07:15 +0600

    HW

---
+++ tz3.py
@@ -0,0 +1,38 @@
+import math
+
+a = []
+for i in range(1, 26):
+    a.append(i)
+
+
+maxChet = 0
+maxNeChet = 0
+for j in a:
+    if (j%2==0 and j>maxChet):
+        maxChet = j
+    if (j%2!=0 and j>maxNeChet):
+        maxNeChet = j
+
+# print(maxChet)
+# print(maxNeChet)
+
+numberMin = min(maxChet, maxNeChet)
+numberMax = max(maxChet, maxNeChet)
+c = []
+
+for k in range(1,numberMin):
+    c.append(k)
+
+for k in range(numberMax, 25):
+    c.append(k)
+
+counter = 0
+arithmeticAverage =0
+for i in c:
+    arithmeticAverage += k
+    counter += 1
+
+print(c)
+print(maxChet)
+print(maxNeChet)
+print(arithmeticAverage/counter)
